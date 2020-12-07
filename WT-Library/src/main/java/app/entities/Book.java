660c88892f7eba1afef52d6c02457ca084faf11a
Giorno6Hackerman <59137523+Giorno6Hackerman@users.noreply.github.com>
2020-12-08 01:16:35 +0300

    Fixed book class

--- WT-Library/src/main/java/app/entities/Book.java
+++ WT-Library/src/main/java/app/entities/Book.java
@@ -1,19 +1,24 @@
 package app.entities;
 
-public class Book {
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Book implements Serializable {
     private String name;
     private String author;
     private String genre;
-    private int year;
+    private int count;
+    private int id;
 
     public Book(){
     }
 
-    public Book(String name, String author, String genre, int year){
+    public Book(String name, String author, String genre, int count, int id){
         this.name = name;
         this.author = author;
         this.genre = genre;
-        this.year = year;
+        this.count = count;
+        this.id = id;
     }
 
     public String getName(){
@@ -40,11 +45,46 @@ public class Book {
         this.genre = genre;
     }
 
-    public int getYear(){
-        return year;
+    public int getCount(){
+        return count;
     }
 
-    public void setYear(int year){
-        this.year = year;
+    public void setCount(int count){
+        this.count = count;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public void setId(int id){
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (!(o instanceof Book))
+            return false;
+        Book book = (Book) o;
+        return id == book.id && name.equals(book.name) && author.equals(book.author) && genre.equals(book.genre)
+                && count == book.count;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, author, genre, count);
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", author='" + author + '\'' +
+                ", genre='" + genre + '\'' +
+                ", count=" + count +
+                '}';
     }
 }
