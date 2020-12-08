aa8a657a28442005c440cced7f078090cab8a9c6
Giorno6Hackerman <59137523+Giorno6Hackerman@users.noreply.github.com>
2020-12-08 10:12:05 +0300

    Added book adding

--- WT-Library/src/main/java/app/model/dao/BookDAO.java
+++ WT-Library/src/main/java/app/model/dao/BookDAO.java
@@ -3,13 +3,33 @@ package app.model.dao;
 import app.entities.Book;
 import app.model.dao.exception.DAOException;
 import app.model.dao.interfaces.IBookDAO;
+import app.model.dao.pool.ConnectionPool;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 
 public class BookDAO implements IBookDAO {
     @Override
     public void addBook(Book book) throws DAOException {
+        Connection connection = null;
 
+        try{
+            connection = ConnectionPool.getInstance().getConnection();
+
+            var addBookRequest = "INSERT INTO books (name, author, genre, count) VALUES (?, ?, ?, ?)";
+            PreparedStatement ps = connection.prepareStatement(addBookRequest);
+            ps.setString(1, book.getName());
+            ps.setString(2, book.getAuthor());
+            ps.setString(3, book.getGenre());
+            ps.setInt(4, book.getCount());
+            ps.executeUpdate();
+        } catch(SQLException ex) {
+            throw new DAOException(ex);
+        } finally {
+            ConnectionPool.getInstance().addConnection(connection);
+        }
     }
 
     @Override
--- WT-Library/src/main/java/app/model/dao/pool/ConnectionPool.java
+++ WT-Library/src/main/java/app/model/dao/pool/ConnectionPool.java
@@ -1,5 +1,55 @@
 package app.model.dao.pool;
 
-public class ConnectionPool {
+import app.model.dao.exception.DAOException;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.LinkedList;
+
+public class ConnectionPool {
+    private static ConnectionPool instance;
+    private static final LinkedList<Connection> connections =new LinkedList<Connection>();
+    private static final int size = 4;
+
+    private ConnectionPool() {}
+
+    public static ConnectionPool getInstance() throws DAOException {
+        if(instance == null)
+        {
+            try{
+                InitialContext initialContext = new InitialContext();
+                Context envContext = (Context) initialContext.lookup("java:/comp/env");
+                DataSource ds = (DataSource) envContext.lookup("jdbc/library");
+
+                for (int i = 0; i < size; i++) {
+                    connections.add(ds.getConnection());
+                }
+
+                instance = new ConnectionPool();
+            } catch (SQLException | NamingException ex) {
+                throw new DAOException(ex);
+            }
+        }
+        return instance;
+    }
+
+    public Connection getConnection() {
+        while(connections.size() == 0) {
+            Thread.yield();
+        }
+
+        Connection connection = connections.stream().findFirst().orElseThrow();
+        connections.remove(connection);
+        return connection;
+    }
+
+    public void addConnection(Connection connection) {
+        if(!connections.contains(connection)) {
+            connections.add(connection);
+        }
+    }
 }
